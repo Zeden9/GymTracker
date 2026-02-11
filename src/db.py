f0eec6d20c1d42671d8a61b9160a18afb45652ec
Zeden9 <116673496+Zeden9@users.noreply.github.com>
2026-02-12 00:36:27 +0100

    Docker and db config

---
+++ src/db.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+from config import DB_CONFIG
+
+
+def get_connection():
+    return psycopg2.connect(**DB_CONFIG)
+
+
+def test_connection():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT version();")
+    version = cur.fetchone()
+    print(f"Connected to :{version}")
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    test_connection()
